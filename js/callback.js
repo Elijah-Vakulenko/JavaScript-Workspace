0ecc5155368de9c1ede2c96753f82b380ff4e5b7
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-04-09 23:21:32 +0100

    Update callback.js

---
+++ js/callback.js
@@ -0,0 +1,41 @@
+// const doMath = function (a, b, callback) {
+//     const result = callback(a, b);
+    
+//     console.log(result);
+// };
+
+// const add = function (x, y) {
+//     return x - y;
+// }
+
+// const sub = function (x, y) {
+//     return x - y;
+// }
+
+// doMath(2, 3, add);//5 ()
+// doMath(10, 3, sub);//2
+
+// //--------------------------------------------------
+// // inline function
+// const doMath = function (a, b, callback) {
+//     const result = callback(a, b);
+    
+//     console.log(result);
+// };
+
+// doMath(2, 3, function (x, y) {
+//     return x + y;
+// });
+
+// doMath(10, 8, function (x, y) {
+//     return x - y;
+// })
+//--------------------------
+// const buttonRef = document.querySelector('.js-button');
+
+// const handleBtnClick = function () {
+//     console.log('click on button' + Date.now());
+// };
+
+// buttonRef.addEventListener('click', handleBtnClick);
+//-----------------------------------------
