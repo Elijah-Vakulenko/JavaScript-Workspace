8d8194d63304907ff3e11c474554fb83ee028f13
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-04-25 18:40:12 +0100

    some edition

---
+++ js/dom.js
@@ -0,0 +1,72 @@
+
+// getting tag by id
+
+// const btn = document.querySelector("#btn-click");
+// console.log(btn);
+
+//----------------------------
+
+// getting tag by class
+// const btn = document.querySelector(".btn");
+// console.log(btn);
+//-------------------------
+
+// getting tag by tag
+
+// const list = document.querySelector("ul");
+// console.log(list);
+
+//-------------------------
+
+// const alllists = document.querySelectorAll("ul");
+// console.log(alllists);
+
+//-------------
+// get an item of the list
+
+// const list = document.querySelector("ul");
+// console.log(list.firstElementChild);
+
+// const secondList = document.querySelector(".list2");
+// console.log(second-list.lastElementChild);
+// console.log(second-list.children);
+
+
+// const list = document.querySelector("ul");
+// const beatle = list.children[2];
+// console.log(beatle);
+// console.log(beatle.nextElementSibling);
+// console.log(beatle.parentNode);
+//--------------
+
+// how to work with image
+
+// const sumy = document.querySelector(".sumy-img");
+// console.log(sumy.src);
+
+// sumy.src = "../img/i-love.jpg";
+// sumy.alt = "sumy logo";
+
+//------------------
+
+// const titleEl = document.querySelector(".title");
+// console.log(titleEl.textContent);
+
+// titleEl.textContent = "New big massive superduper title";
+//------------------
+// const sumy = document.querySelector(".sumy-img");
+// console.log(sumy.src);
+// console.log(sumy.getAttribute("src"));
+// sumy.setAttribute("Alt", "HOMETOWN");
+
+// sumy.removeAttribute("width");
+
+// console.log(sumy.hasAttribute("height"));
+// console.log(sumy.hasAttribute("alt"));
+
+//---------------------------
+
+// data-
+
+const actions = document.querySelectorAll(".actions button");
+console.log(actions);
