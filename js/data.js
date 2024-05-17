1de3a3a37fd3bdc13703e9d6a5f0a166a70637a4
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-05-17 19:57:39 +0100

    edit

--- js/data.js
+++ js/data.js
@@ -87,70 +87,320 @@
 // }, 2000)
 //---------
 
-const startBtn = document.querySelector("button[data-action-start]")
-const stopBtn = document.querySelector("button[data-action-stop]");
-const clockface = document.querySelector(".clockface");
+// const startBtn = document.querySelector("button[data-action-start]")
+// const stopBtn = document.querySelector("button[data-action-stop]");
+// const clockface = document.querySelector(".clockface");
 
-class Timer {
+// class Timer {
 
-    constructor({onTick}) {
-        this.isActive = false;
-        this.onTick = onTick;
-        this.intervalId();
-        this.init();
-    }
+//     constructor({onTick}) {
+//         this.isActive = false;
+//         this.onTick = onTick;
+//         this.intervalId();
+//         this.init();
+//     }
 
-    init() {
-        const time = this.getTimeComponent(0);
-        this.onTick(time);
-    }
+//     init() {
+//         const time = this.getTimeComponent(0);
+//         this.onTick(time);
+//     }
 
-    start() {
-        if (this.isActive) {
-            return
+//     start() {
+//         if (this.isActive) {
+//             return
+//         }
+//         const startTime = Date.now();
+//         this.isActive = true;
+        
+//         setInterval(() => {
+//             const currentTime = Date.now();
+
+//             const deltaTime = currentTime - startTime;
+
+//             const time = this.getTimeComponent(deltaTime);
+//        this.onTick(time)
+//         }, 1000)
+//     }
+
+//     stop() {
+//         clearInterval(this.intervalId);
+//         const time = this.getTimeComponent(0);
+//         this.onTick(time);
+
+//         this.isActive = false;
+//     }
+
+//     getTimeComponent(time) {
+//         const seconds = this.pad(Math.floor((time % (1000 * 60)) / 1000););
+//         const minutes = this.pad(Math.floor(time % (1000 * 60 * 60) / (1000 * 60)));
+//         const hours = this.pad(Math.floor(time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+        
+//         return {hours, minutes, seconds}
+//     }
+
+//     pad(value) {
+//         return String(value).padStart(2, "0");
+//     }
+// }
+
+// const timer = new Timer({
+//     onTick: updateClockFace,
+// });
+
+// startBtn.addEventListener('click', timer.start.bind(timer));
+// stopBtn.addEventListener("click", timer.stop.bind(timer));
+
+// function updateClockFace({ hours, minutes, seconds }) {
+//     clockface.textContent = `${hours}:${minutes}:${seconds}`;
+// }
+
+//--------------------------------------
+
+const prom = new Promise((resolve, reject) => {
+    const random = Math.random();
+
+    setTimeout(() => {
+        if (random > 0.5) {
+            resolve("fulfield");
+        } else {
+            reject("rejected");
         }
-        const startTime = Date.now();
-        this.isActive = true;
-        
-        setInterval(() => {
-            const currentTime = Date.now();
-
-            const deltaTime = currentTime - startTime;
-
-            const time = this.getTimeComponent(deltaTime);
-       this.onTick(time)
-        }, 1000)
-    }
-
-    stop() {
-        clearInterval(this.intervalId);
-        const time = this.getTimeComponent(0);
-        this.onTick(time);
-
-        this.isActive = false;
-    }
-
-    getTimeComponent(time) {
-        const seconds = this.pad(Math.floor((time % (1000 * 60)) / 1000););
-        const minutes = this.pad(Math.floor(time % (1000 * 60 * 60) / (1000 * 60)));
-        const hours = this.pad(Math.floor(time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-        
-        return {hours, minutes, seconds}
-    }
-
-    pad(value) {
-        return String(value).padStart(2, "0");
-    }
-}
-
-const timer = new Timer({
-    onTick: updateClockFace,
+    }, 3000)
 });
 
-startBtn.addEventListener('click', timer.start.bind(timer));
-stopBtn.addEventListener("click", timer.stop.bind(timer));
+console.log(prom);
 
-function updateClockFace({ hours, minutes, seconds }) {
-    clockface.textContent = `${hours}:${minutes}:${seconds}`;
+//-------------
+
+prom
+
+    .then((data) => {
+        console.log("then", data);
+    })
+
+    .catch((error) => {
+        console.log("catch", error);
+    })
+
+    .finally(() => console.log("finally"));
+
+    //-----------------
+
+const lalala = new Promise((resolve, reject) => {
+        resolve(5)
+})
+    
+lalala
+    .then(res => {
+        console.log(res);
+        return res * 2;
+    })
+    .then(data => {
+        console.log(data);
+        return data + 1;
+
+    })
+    .then(res => {
+        console.log(res);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    .finally(() => { })
+    
+    //--------
+const promise = new Promise((resolve) => {
+        resolve(10)
+})
+    
+promise
+    .then(value => {
+        return new Promise((resolve) => {
+            resolve(value * 2)
+        })
+    })
+    .then(value => console.log(value))
+
+//---------------
+
+console.log("step 1");
+
+setTimeout(() => {
+    console.log("step 2");
+}, 0)
+
+new Promise((resolve) => {
+    resolve("step 3")
+})
+    .then(data => console.log(data))
+
+setTimeout(() => {
+    console.log("step 4");
+});
+
+//--------------
+
+const makeOrder = (dish, onSuccess, onError) => {
+    const random = Math.random();
+
+    setTimeout(() => {
+        if (random > 0.5) {
+            onSuccess(`your order ${dish}`)
+        } else {
+            onError("item is out of stock");
+        }
+    }, 1000);
+}
+
+makeOrder(
+    "Pie",
+    (str) => console.log(str);
+    (error) => console.log(error);
+)
+
+//------------------------------
+
+const makeOrder = (dish) => {
+    return new Promise((resolve, reject) => {
+        const random = Math.random();
+
+        setTimeout(() => {
+            if (random > 0.5) {
+               resolve (`your order ${dish}`)
+            } else {
+                reject ("the item is out of stock")
+           }
+        }, 1000);
+    })
+}
+
+makeOrder("Pie")
+    .then(data => console.log(data))
+    .catch(error => console.log(error);)
+
+    //---------------
+
+const makeOrder = (dish) => {
+    const random = Math.random();
+
+    if (random > 0.5) {
+            return Promise.resolve(`your order ${dish}`)
+    } else {
+        return Promise.reject("item is out of stock")
+        }
+}
+
+makeOrder("Pie")
+.then(data => console.log(res))
+    .catch(error => console.log(error);)
+
+
+    //----------------
+
+
+
+const data1 = Date.now()
+
+const res1 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const date2 = Date.now()
+            const delta = date2 - date1;
+
+            resolve({title: "first", time: delta})
+        }, 2000);
+    })
+}
+
+const res2 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const date2 = Date.now()
+            const delta = date2 - date1;
+
+            // resolve({ title: "second", time: delta })
+            reject("error lalala")
+        }, 1000);
+    })
+}
+
+const res3 = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const date2 = Date.now()
+            const delta = date2 - date1;
+
+            resolve({title: "third", time: delta})
+        }, 1500);
+    })
+}
+
+Promise.all([res1(), res2(), res3()])
+    .then(data => console.log(data))
+    .catch(error => console.log("error", error));
+
+    //---------------
+
+Promise.race([res1(), res2(), res3()])
+    .then(lalala => console.log(lalala))
+    .catch(error => console.log(error));
+
+    //--------------------
+
+const startBtn = document.querySelector(".start-btn");
+const contaoner = document.querySelector(".container");
+const result = document.querySelector(".result");
+
+startBtn.addEventListener('click', handleStart);
+
+function handleStart() {
+
+    startBtn.disabled = true;
+    result.innerHTML = "";
+    
+    const promises = [...container.children].map(() => {
+        return new Promise((resolve, reject) => {
+            const random = Math.random();
+            if (random > 0.5) {
+                resolve('0_0')
+            } else {
+                reject('=P');
+            }
+        })
+    })
+
+    Promise.allSettled(promises)
+        .then(data => {
+
+            const isWinner =
+                data.every((item) => item.status === "fulfilled") ||
+                data.every((item) => item.status === "rejected") 
+            
+            result.innerHTML = isWinner ? "Winner" : "Loser";
+
+            data.forEach((item, i) => {
+
+                container.children[i].textContent = "";
+
+                setTimeout(() => {
+                    container.children[i].textContent = item.value || item.reason;
+
+                    if (i === data.length - 1) {
+                        result.innerHTML = isWinner ? "Winner" : "Loser";
+                        startBtn.disabled = false;
+                    }
+                     
+                }, 1000 * (i + 1));
+                
+               
+                
+                // container.children[i].textContent = item.value || item.reason;
+            })
+        })
+    
+    
+    new Promise((resolve, reject) => {
+
+    })
 }
 
